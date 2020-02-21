b8d4146ec8969b10bde21af373ba69c5b168680e
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-02-21 14:00:27 +0100

    chore: purgecss ignore

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,8 @@
 </template>
 
 <style lang="postcss">
+/* purgecss start ignore */
+
 @import 'https://fonts.googleapis.com/icon?family=Material+Icons';
 
 html,
@@ -15,4 +17,6 @@ body {
 .material-icons {
   vertical-align: middle;
 }
+
+/* purgecss end ignore */
 </style>
