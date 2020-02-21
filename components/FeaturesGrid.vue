3b60e1efdf5e23d32b403b2a50bae2badeefde02
Marcus Lyons <10541922+marcuslyons@users.noreply.github.com>
2020-02-21 09:26:02 -0700

    Update FeaturesGrid.vue
    Updated copy for the Project manager feature.

--- components/FeaturesGrid.vue
+++ components/FeaturesGrid.vue
@@ -18,7 +18,7 @@ export default {
       <FeatureItem
         title="Project manager"
       >
-        Create and manager all your JS projects
+        Create and manage all of your JS projects
       </FeatureItem>
 
       <FeatureItem
